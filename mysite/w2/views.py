c58e00d1d190fe333de9f1eb517f9be3ca9d5297
NeoRamCreator <gawriil.mu@yandex.ru>
2021-10-29 02:35:21 +0300

    29_10

--- mysite/w2/views.py
+++ mysite/w2/views.py
@@ -9,20 +9,20 @@ def index(request):
     return render(request, 'w2/index.html', {})
 
 
-# class PersonCreateView(CreateView):
-#     # models = CreatePerson
-#     # fields = ['title', 'text']
-#     # fields = '__all__'
-#     template_name = 'w2/index.html'
-#     form_class = CreatePerson
-#
-#
-# class PersonListenView(ListView):
-#     model = Person
-#     template_name = 'w2/index.html'
-
-
-class All(CreateView, ListView):
-    model = Person
+class PersonCreateView(CreateView):
+    # models = CreatePerson
+    # fields = ['title', 'text']
+    # fields = '__all__'
     template_name = 'w2/index.html'
     form_class = CreatePerson
+
+
+class PersonListenView(ListView):
+    model = Person
+    template_name = 'w2/index.html'
+
+
+# class All(CreateView, ListView):
+#     model = Person
+#     template_name = 'w2/index.html'
+#     form_class = CreatePerson
