609604f252f320e234edbb22c5238983e0b28714
NeoRamCreator <gawriil.mu@yandex.ru>
2021-11-02 21:32:34 +0300

    главная страница

--- mysite/w2/forms.py
+++ mysite/w2/forms.py
@@ -1,15 +1,19 @@
 from django import forms
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, TextInput
 
 from .models import Person
 
 
 class CreatePerson(ModelForm):
-
     class Meta:
         model = Person
         fields = ['title', 'text', 'img']
 
+        # widgets = {
+        #     'text': Textarea(attrs={'cols': 30, 'rows': 5}),
+        # }
+
         widgets = {
-            'text': Textarea(attrs={'cols': 30, 'rows': 5}),
+            'text': TextInput(attrs={'class': 't2'}),
         }
+
